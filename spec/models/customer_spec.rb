f13df5ec6b8b8344e05958030d2a258e3f3b1ec7
LucasPascale <lpluke5@gmail.com>
2022-09-15 12:21:41 -0600

    rspec/models: Setup framework for model testing

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Customer do
+  context 'relationships' do
+
+  end
+
+  context 'validations' do
+
+  end
+end
---
+++ spec/models/tea_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Tea do
+  context 'relationships' do
+    
+  end
+
+  context 'validations' do
+    
+  end
+end 
