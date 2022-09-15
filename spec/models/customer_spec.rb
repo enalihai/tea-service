d0e5860254622919314d5ad67814cc4b09b8b75c
LucasPascale <lpluke5@gmail.com>
2022-09-15 12:24:13 -0600

    customer/model: Setup RSpec for customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Customer do
   context 'relationships' do
-
+    it { should have_many :subsciptions }
+    it { should have_many(:teas).through(:subscriptions) }
   end
 
   context 'validations' do
-
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
   end
 end
