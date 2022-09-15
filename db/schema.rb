e5a93cce78204a1df5724ab7e406163f91c0c5a4
LucasPascale <lpluke5@gmail.com>
2022-09-15 12:16:47 -0600

    migrations/models: Setup model migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_173628) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_181143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_173628) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "tea_id", null: false
+    t.bigint "customer_id", null: false
+    t.string "title"
+    t.integer "price"
+    t.integer "status"
+    t.integer "frequency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
+  end
+
   create_table "teas", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -32,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_173628) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
