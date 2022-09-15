50d1878a4da20dda92abe1f5806f1f9aaabc66de
LucasPascale <lpluke5@gmail.com>
2022-09-15 12:28:29 -0600

    tea/model: Setup RSpec for tea model

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Tea do
   context 'relationships' do
-    
+    it { should have_many :subscriptions }
+    it { should have_many(:customers).through(:subscriptions)}
   end
 
   context 'validations' do
-    
+    it { should_validate_presence_of :title }
+    it { should_validate_presence_of :description }
+    it { should_validate_presence_of :temperature }
+    it { should_validate_presence_of :brew_time }
   end
 end 
