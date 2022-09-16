4539e45bd1068c55a5af5e72ec1e10117220d312
LucasPascale <lpluke5@gmail.com>
2022-09-15 22:02:03 -0600

    serializer/subs: Setup serializer for use in Subs controller

---
+++ app/serializers/api/v1/subscription_serializer.rb
@@ -0,0 +1,16 @@
+class Api::V1::SubscriptionSerializer
+  def self.sub_json(new_sub)
+    {
+      data: {
+        id: new_sub.id.to_s,
+        type: 'subscription',
+        attributes: {
+          title: new_sub.title,
+          price: new_sub.price,
+          frequency: new_sub.frequency,
+          status: new_sub.status
+        }
+      }
+    }
+  end
+end
