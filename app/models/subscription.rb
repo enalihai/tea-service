939568bb55e510d8e00e45ae42d12f963bbaeda4
LucasPascale <lpluke5@gmail.com>
2022-09-15 22:01:06 -0600

    refactor/models: Add enum tests and class methods

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,4 +7,25 @@ class Subscription < ApplicationRecord
 
   enum status: [:active, :cancelled]
   enum frequency: [:weekly, :monthly, :yearly]
+
+  def self.new_subscription(frequency, customer, tea)
+    new_subscription_hash = {
+      customer_id: customer.id,
+      tea_id: tea.id,
+      frequency: frequency,
+      title: "#{tea.title} = #{frequency} subscription for #{customer.first_name}",
+      status: 'active'
+    }
+
+    case frequency
+      when 'weekly'
+        new_subscription_hash[:price] = 400
+      when 'monthly'
+        new_subscription_hash[:price] = 1100
+      when 'yearly'
+        new_subscription_hash[:price] = 12000
+    else; end
+
+    new(new_subscription_hash)
+  end
 end
